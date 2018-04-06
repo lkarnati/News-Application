38472faf20b75adb47e50ccdce4eba0e23895cc5
sbeesabathuni <sbeesabathun@cs.stonybrook.edu>
2018-04-06 16:25:03 -0400

    Multi select dropdown - fixing reset on reload.

--- public/news/SourceController.js
+++ public/news/SourceController.js
@@ -30,6 +30,7 @@ angular.module("newsapp.news").controller("sourceController", sourceController),
                     if ($rootScope.selectedSourceList == null) {
                         $rootScope.selectedSourceList = $localStorage.selectedSourceList;
                     }
+                    //console.log($rootScope.selectedSourceList);
                     if (typeof $rootScope.selectedSourceList !== "undefined" && $rootScope.selectedSourceList.length > 0) {
                         for (i = 0; i < $rootScope.selectedSourceList.length; i++) {
                             for (j = 0; j < sourceCtrl.sourceList.length; j++) {
@@ -47,16 +48,20 @@ angular.module("newsapp.news").controller("sourceController", sourceController),
                    console.log(error);
                  });
         }
-
+        // on reset or select none
         function resetSourceList() {
+            for (j = 0; j < sourceCtrl.sourceList.length; j++) {
+                 sourceCtrl.sourceList[j]['ticked'] = false;
+            }
             sourceCtrl.selectedSourceList=[];
             sourceCtrl.isValid = true;
-            $rootScope.selectedSourceList = sourceCtrl.selectedSourceList;
-            $localStorage.selectedSourceList = sourceCtrl.selectedSourceList;
+            $rootScope.selectedSourceList = [];
+            $localStorage.selectedSourceList = [];
         }
+
         // set the tiles on selection and if number of selected values is less than 5
         function selectSourceList(selectedSourceList) {
-            console.log(sourceCtrl.selectedSourceList);
+            //console.log(sourceCtrl.selectedSourceList);
             if(sourceCtrl.selectedSourceList.length > 5) {
                 sourceCtrl.isValid = false;
                 return;
